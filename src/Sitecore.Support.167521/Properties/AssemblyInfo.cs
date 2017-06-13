6c2f7b1ca6de42ba00ddfb6c4b0d4f29ce19c8d7
ido <ido@WS-IDO1UA1>
2017-06-13 14:50:01 +0300

    First commit - patch is ready

---
+++ src/Sitecore.Support.167521/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.167521")]
+[assembly: AssemblyProduct("Sitecore.Support.167521")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyDescription("Error after refreshed an Experience Editor MVC page with -Associated content- checkbox selected")]
+
